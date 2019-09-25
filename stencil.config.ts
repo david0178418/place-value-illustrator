19b0435c1ab65a25e2e92e416db3dc005270c010
David Granado <davidjgranado@gmail.com>
2019-09-25 11:09:36 -0500

    Manually adjust base url for site deployment

--- stencil.config.ts
+++ stencil.config.ts
@@ -10,7 +10,7 @@ export const config: Config = {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: '/'
+      baseUrl: 'https://davidgranado.com/projects/counter/'
     }
   ]
 };
